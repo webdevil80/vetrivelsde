647c0f9721883a9a2d9f43e231db804838d0bfa0
webdevil80 <69701204+webdevil80@users.noreply.github.com>
2025-11-01 16:00:57 +0530

    Update and rename main.jsx to main.js

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  React.createElement(React.StrictMode, null, React.createElement(App, null))
+);
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App.js';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
