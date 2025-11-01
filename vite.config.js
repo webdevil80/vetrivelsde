60ac6a713dac73386bc90016e21a777c9984eb83
webdevil80 <69701204+webdevil80@users.noreply.github.com>
2025-11-01 14:58:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/vetri-portfolio/', // Replace <repository-name> with your repository name
+  base: '/vetrivelsde/', // Replace <repository-name> with your repository name
 
 });
+
